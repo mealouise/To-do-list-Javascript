37964e82d899877a908eba2d33f49e2683c35a0f
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-03-12 15:45:24 +0000

    styling added

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const addButton = document.getElementById("add");
 
 let todos = [];
 
+// let today = new Date();
+// let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+// console.log(date);
+// date = date.split(' ')[0];
+// document.getElementById("todaysdate").innerHTML = today;
+
+// console.log(date);
+
+
 const add = () => {
     const task = document.getElementById('task').value;
     todos.push(task);
@@ -15,23 +24,42 @@ const remove = (id) => {
     return false;
 }
 
+// const refreshArray
+
+
+// const editLi = () => {
+//     todos[i].innerHTML = inputText.value;
+// }
 
 const show = () => {
     let html = '<ul>';
     for (let i=0; i<todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i + '">X</button></li>';
+        html += '<li class="checked">' + todos[i] + '<button class="remove" id="' + i + '">X</button></li>';
     };
     html += '</ul>';
     
     document.getElementById('todos').innerHTML = html
 
-    const buttons = document.getElementsByClassName('remove');
-    for (let i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener('dblclick', () => {
+const removeButton = document.getElementsByClassName('remove');
+    for (let i=0; i < removeButton.length; i++) {
+        removeButton[i].addEventListener('click', () => {
             remove(i)
         });
     };
 }
+
+const editButton = document.getElementsByClassName('edit');
+    for (let i=0; i < editButton.length; i++) {
+        editButton[i].addEventListener('click', () => {
+        // style.textDecoration = "line-through";  
+        });
+    }
+
+
+
+
+
 addButton.addEventListener("click", () => {
     add();
 })
+
